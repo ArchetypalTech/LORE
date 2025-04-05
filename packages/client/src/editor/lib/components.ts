c83cc12c3db595fcbbc38fa819ab9b3e07cf024f
Alexander Mangel <cygnusfear@gmail.com>
2025-04-05 10:49:50 +0200

    refactor: componet location
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>

--- packages/client/src/editor/lib/components.ts
+++ packages/client/src/editor/lib/components.ts
@@ -1,17 +1,64 @@
-import {
-	type EntityCollection,
-	type ComponentInspector,
-	type WithStringEnums,
-	createDefaultEntity,
-	createDefaultAreaComponent,
-	createDefaultInspectableComponent,
-	createDefaultExitComponent,
+import type {
+	EntityCollection,
+	ComponentInspector,
+	WithStringEnums,
 } from "@/editor/lib/schemas";
-import type { Entity } from "@/lib/dojo_bindings/typescript/models.gen";
+import type {
+	Entity,
+	SchemaType,
+} from "@/lib/dojo_bindings/typescript/models.gen";
 import { AreaInspector } from "../components/inspectors/AreaInspector";
 import { EntityInspector } from "../components/inspectors/EntityInspector";
 import { ExitInspector } from "../components/inspectors/ExitInspector";
 import { InspectableInspector } from "../components/inspectors/InspectableInspector";
+import randomName from "@scaleway/random-name";
+import { randomKey } from "../editor.utils";
+
+export const createDefaultEntity = (): WithStringEnums<
+	Pick<SchemaType["lore"], "Entity">
+> => ({
+	Entity: {
+		inst: randomKey(),
+		is_entity: true,
+		name: randomName(),
+		alt_names: [],
+	},
+});
+
+export const createDefaultAreaComponent = (
+	entity: Entity,
+): WithStringEnums<Pick<SchemaType["lore"], "Area">> => ({
+	Area: {
+		inst: entity.inst,
+		is_area: true,
+		direction: "None",
+	},
+});
+
+export const createDefaultInspectableComponent = (
+	entity: Entity,
+): WithStringEnums<Pick<SchemaType["lore"], "Inspectable">> => ({
+	Inspectable: {
+		inst: entity.inst,
+		is_inspectable: true,
+		is_visible: true,
+		description: [],
+		action_map: [],
+	},
+});
+
+export const createDefaultExitComponent = (
+	entity: Entity,
+): WithStringEnums<Pick<SchemaType["lore"], "Exit">> => ({
+	Exit: {
+		inst: entity.inst,
+		is_exit: true,
+		is_enterable: true,
+		leads_to: 0,
+		direction_type: "None",
+		action_map: [],
+	},
+});
 
 export const componentData: {
 	[K in keyof EntityCollection]: {
