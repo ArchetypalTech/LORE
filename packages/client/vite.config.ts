a461c7dcccf3b3b7333efcc855563f1564fb632c
Alexander Mangel <cygnusfear@gmail.com>
2025-04-10 21:39:34 +0200

    fix: clean up package files, ci command
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { bgGreen, black } from "ansicolor";
 import { defineConfig, loadEnv } from "vite";
 import mkcert from "vite-plugin-mkcert";
