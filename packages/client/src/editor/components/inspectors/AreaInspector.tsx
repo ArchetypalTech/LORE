2634363309fd37ac42bf832c4ac7674531e616ed
Dkrenon <bgsbonilla@gmail.com>
2025-04-23 12:06:55 +0100

    chore: removed commented area in `AreaInspector`, readded tests in `dev.watch`

--- packages/client/src/editor/components/inspectors/AreaInspector.tsx
+++ packages/client/src/editor/components/inspectors/AreaInspector.tsx
@@ -1,9 +1,6 @@
-// import { stringCairoEnum } from "@/editor/lib/schemas";
 import {
 	type Area,
-	//direction,
 } from "@/lib/dojo_bindings/typescript/models.gen";
-// import { CairoEnumSelect } from "../FormComponents";
 import type { ComponentInspector } from "./useInspector";
 import { useInspector } from "./useInspector";
 
@@ -11,27 +8,15 @@ export const AreaInspector: ComponentInspector<Area> = ({
 	componentObject,
 	...props
 }) => {
-	const { /* handleInputChange, */ Inspector } = useInspector<Area>({
+	const { Inspector } = useInspector<Area>({
 		componentObject,
 		...props,
-		// inputHandlers: {
-		// 	direction: (e, updatedObject) => {
-		// 		console.log(e.target.value);
-		// 		updatedObject.direction = stringCairoEnum(e.target.value);
-		// 	},
-		// },
 	});
 
 	if (!componentObject) return <div>Area not found</div>;
 
 	return (
 		<Inspector>
-			{/* <CairoEnumSelect
-				id="direction"
-				onChange={handleInputChange}
-				value={componentObject.direction}
-				enum={direction}
-			/> */}
 		</Inspector>
 	);
 };
