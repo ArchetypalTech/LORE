5fee0f67383e32042f095a4fb26c7cda149226c4
JCEEE Nicolas <0xjceee@proton.me>
2025-05-29 16:53:16 +0100

    feat: The O'Ruggin Trail title and minor terminal UI enhancement (#66)
    update ORUG title && switch input for textarea

--- packages/client/src/data/app.data.ts
+++ packages/client/src/data/app.data.ts
@@ -4,9 +4,9 @@ import { APP_RANDOM_ICON } from "./app.icons";
 const SLOT_INDICATOR = `<text y='.1em' font-size='90' transform='translate(45, 30) scale(0.5,0.5)'>${LORE_CONFIG.useController ? "✨" : "🎬"}</text>`;
 
 export const APP_DATA = {
-	title: ">LORE",
+	title: ">ORUG",
 	icon: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y='.9em' font-size='90' transform='translate(110, 5) scale(-.975,.975)'>🚪</text>${SLOT_INDICATOR}</svg>`,
-	intro: ["\n", "> LORE", "\n", "Welcome", "\n\n\n\n\n\n\n\n\n\n"].join("\n"),
+	intro: ["\n", "The O'Ruggin Trail", "\n", "A text based adventure", "\n\n\n\n\n\n\n\n\n\n"].join("\n"),
 	description: "",
 	keywords: [],
 	image: "",
@@ -14,7 +14,7 @@ export const APP_DATA = {
 };
 
 export const APP_EDITOR_DATA = {
-	title: ">LORED",
+	title: ">ORUG",
 	icon: () =>
 		`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y='.9em' font-size='90' transform='translate(110, 5) scale(-.975,.975)'>${APP_RANDOM_ICON()}</text>${SLOT_INDICATOR}</svg>`,
 	description: "",
