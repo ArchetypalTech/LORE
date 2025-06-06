dd30b8febb7032748180db1de3d75b20b228cb13
okhai <57156589+okhaimie-dev@users.noreply.github.com>
2025-06-06 14:57:55 -0500

    feat: sync fix (#81)
    * wip: sync fix
    
    * Add component merging to entity data sync
    
    ---------
    
    Signed-off-by: okhai <57156589+okhaimie-dev@users.noreply.github.com>

--- packages/client/src/lib/stores/wallet.store.ts
+++ packages/client/src/lib/stores/wallet.store.ts
@@ -83,6 +83,22 @@ const setupController = async () => {
 					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
 				},
 				{
+					entrypoint: "create_condition",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
+					entrypoint: "create_effect",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
+					entrypoint: "create_action",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
+					entrypoint: "create_trigger",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
 					entrypoint: "create_child",
 					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
 				},
@@ -107,7 +123,23 @@ const setupController = async () => {
 					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
 				},
 				{
+					entrypoint: "delete_condition",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
 					entrypoint: "delete_inventory_item",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
+					entrypoint: "delete_trigger",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
+					entrypoint: "delete_effect",
+					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
+				},
+				{
+					entrypoint: "delete_action",
 					description: `The terminal endpoint for ${APP_EDITOR_DATA.title}`,
 				},
 				{
