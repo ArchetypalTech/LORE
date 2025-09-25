a6babb230d36f4c68f9f84d99f17751abb3b51c8
awtnmy <167875188+awtnmy@users.noreply.github.com>
2025-09-25 20:29:01 +0100

    Update Typewriter.tsx
    Adjusting speed and random delay range
    
    Signed-off-by: awtnmy <167875188+awtnmy@users.noreply.github.com>

--- packages/client/src/client/terminal/Typewriter.tsx
+++ packages/client/src/client/terminal/Typewriter.tsx
@@ -7,8 +7,8 @@ import TerminalLine from "./TerminalLine";
 export default function Typewriter() {
 	const [displayContent, setDisplayContent] =
 		useState<TerminalContentItem | null>(null);
-	const minTypingDelay = 5;
-	const maxTypingDelay = 15;
+	const minTypingDelay = 2;
+	const maxTypingDelay = 9;
 
 	const { activeTypewriterLine } = useTerminalStore();
 
@@ -51,7 +51,7 @@ export default function Typewriter() {
 				});
 				currentIndex++;
 			},
-			(activeTypewriterLine.speed || 10) *
+			(activeTypewriterLine.speed || 6) *
 				Math.random() *
 				(maxTypingDelay - minTypingDelay) +
 				minTypingDelay,
