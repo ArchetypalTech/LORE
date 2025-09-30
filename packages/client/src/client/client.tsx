d65da1ce30d346abb6b591c9e8c0e4a66c0e09c6
JCEEE <0xjceee@proton.me>
2025-09-30 09:25:56 +0100

    rm comment msg when mobile

--- packages/client/src/client/client.tsx
+++ packages/client/src/client/client.tsx
@@ -24,15 +24,15 @@ export const Client = () => {
 	return (
 		<div
 			id="client-root"
-			className="relative flex h-screen w-screen items-center justify-center "
+			className="relative flex h-screen w-screen max-h-[100dvh] items-center justify-center "
 		>
-			<div className="fixed z-[0] opacity-40 w-screen h-screen artwork-background">
+			<div className="fixed z-[0] opacity-40 w-screen h-screen max-h-[100dvh] artwork-background">
 				<img src={bg} alt="oruggin-background" />
 			</div>
 			<div className="crt buzzing flex h-full max-h-[100dvh] md:max-h-[70%] w-full items-center justify-center">
 				<Terminal />
 			</div>
-			<div className="fixed bottom-4">
+			<div className="fixed bottom-4 hidden md:block">
 					<p className="text-center text-xs text-amber-300">Liked the game? Leave a comment on our <a aria-label="leave a comment" className="comments underline" href="https://archetypaltech.itch.io/oruggin-trail">Itch.io</a> page!</p>
 			</div>
 		</div>
