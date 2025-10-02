9e872fd0b6c742760063630372cce6389520e03e
JCEEE <0xjceee@proton.me>
2025-10-02 10:47:18 +0100

    add audio controls compnents

---
+++ packages/client/src/client/terminal/SVGWrap.tsx
@@ -0,0 +1,13 @@
+export default function({ children }) {
+return (
+<svg
+  width="1em"
+  height="1em"
+  fill="none"
+  stroke="gold"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+>{children}</svg>
+)
+}
