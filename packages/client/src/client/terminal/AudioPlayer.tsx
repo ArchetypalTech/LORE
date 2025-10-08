2620108015e39be47a7a80780b8a7aa14a9d86bb
Dkrenon <bgsbonilla@gmail.com>
2025-10-08 17:14:54 +0100

    chore: replace url with local files for now

--- packages/client/src/client/terminal/AudioPlayer.tsx
+++ packages/client/src/client/terminal/AudioPlayer.tsx
@@ -2,30 +2,37 @@ import { useEffect, useRef, useState } from "react";
 import { useDojoStore } from "@/lib/stores/dojo.store";
 import { useTerminalStore } from "@/lib/stores/terminal.store";
 
-// Tracks link
-const tracks = [
-	//"https://docs.google.com/uc?export=download&id=1tDIy7xg6GdHTm-foUHd5GjuZSB7kv3zf", // I'm your Guy (instrumental)
-	"https://drive.google.com/file/d/1tDIy7xg6GdHTm-foUHd5GjuZSB7kv3zf/view",
-	//"https://docs.google.com/uc?export=download&id=1SSeTiUGTDFfbVoFWoFzWpRtMiEkWtUmr", // The Belly of the Earth (instrumental)
-	"https://drive.google.com/file/d1SSeTiUGTDFfbVoFWoFzWpRtMiEkWtUmr/view",
-	//"https://docs.google.com/uc?export=download&id=186mTlgYxVfhmxYSNA9wIAo_RjmJTAqHv", // Without saying a thing (instrumental)
-	"https://drive.google.com/file/186mTlgYxVfhmxYSNA9wIAo_RjmJTAqHv/view",
-	//"https://docs.google.com/uc?export=download&id=1c57tzTNHxIlM1X_5wpt14otWUKGdSRES", // You're Home (instrumental)
-	"https://drive.google.com/file/1c57tzTNHxIlM1X_5wpt14otWUKGdSRES/view",
-];
+// Tracks link - NOT IN USE CURRENTLY
+// const tracks = [
+// 	//"https://docs.google.com/uc?export=download&id=1tDIy7xg6GdHTm-foUHd5GjuZSB7kv3zf", // I'm your Guy (instrumental)
+// 	"https://drive.google.com/file/d/1tDIy7xg6GdHTm-foUHd5GjuZSB7kv3zf/view",
+// 	//"https://docs.google.com/uc?export=download&id=1SSeTiUGTDFfbVoFWoFzWpRtMiEkWtUmr", // The Belly of the Earth (instrumental)
+// 	"https://drive.google.com/file/d1SSeTiUGTDFfbVoFWoFzWpRtMiEkWtUmr/view",
+// 	//"https://docs.google.com/uc?export=download&id=186mTlgYxVfhmxYSNA9wIAo_RjmJTAqHv", // Without saying a thing (instrumental)
+// 	"https://drive.google.com/file/186mTlgYxVfhmxYSNA9wIAo_RjmJTAqHv/view",
+// 	//"https://docs.google.com/uc?export=download&id=1c57tzTNHxIlM1X_5wpt14otWUKGdSRES", // You're Home (instrumental)
+// 	"https://drive.google.com/file/1c57tzTNHxIlM1X_5wpt14otWUKGdSRES/view",
+// ];
 
-// Random track generator avoiding immediate repeats
 function randomTrack(currentTrack: string) {
-  const otherTracks = tracks.filter(track => track !== currentTrack);
-  return otherTracks[Math.floor(Math.random() * otherTracks.length)];
+	const collection = ["track_1", "track_2", "track_3", "track_4", "track_5"];
+	const otherTracks = collection.filter(track => track !== currentTrack);
+	const trackname = otherTracks[Math.floor(Math.random() * otherTracks.length)];
+	return `/album/${trackname}.mp3`;
 }
+
+// // Random track generator avoiding immediate repeats
+// function randomTrack(currentTrack: string) {
+//   const otherTracks = tracks.filter(track => track !== currentTrack);
+//   return otherTracks[Math.floor(Math.random() * otherTracks.length)];
+// }
 
 export default function () {
 	// start with random track
 	//const [useTrack, setTrack] = useState(randomTrack());
 	// Start with "The Belly of the Earth"
   const [useTrack, setTrack] = useState(
-    "https://drive.google.com/file/d1SSeTiUGTDFfbVoFWoFzWpRtMiEkWtUmr/view"
+    "/album/track_3.mp3"
   );
 	// to know when to start randoms tracks
   const [hasPlayedIntro, setHasPlayedIntro] = useState(false); 
@@ -40,7 +47,7 @@ export default function () {
   const handleLoadedMetadata = () => {
     if (
       useTrack ===
-        "https://drive.google.com/file/d1SSeTiUGTDFfbVoFWoFzWpRtMiEkWtUmr/view" &&
+        "/album/track_3.mp3" &&
       audioRef.current
     ) {
       audioRef.current.currentTime = 65; // start at 1:05
@@ -53,7 +60,7 @@ export default function () {
     if (audioRef.current && enableAudio) {
       audioRef.current.volume = volumeAudio;
       // Play normally if not “The Belly of the Earth”
-      if (useTrack !== "https://drive.google.com/file/d1SSeTiUGTDFfbVoFWoFzWpRtMiEkWtUmr/view") {
+      if (useTrack !== "/album/track_3.mp3") {
         audioRef.current.play();
       }
     }
