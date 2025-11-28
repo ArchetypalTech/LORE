8f50436ef40860cd2f72c7e55e667a0ca04a1672
Dkrenon <bgsbonilla@gmail.com>
2025-11-28 11:42:03 +0000

    chore: fix display of editor footer for trailer

--- packages/client/src/editor/editor.tsx
+++ packages/client/src/editor/editor.tsx
@@ -208,7 +208,11 @@ export const Editor = () => {
 	return (
 		<>
 			<Toaster expand visibleToasts={4} position="top-left" />
-			{/* Fullscreen idle video overlay — now in front of terminal */}
+			<div
+				id="editor-root"
+				className="fixed h-screen max-h-screen w-full overflow-scroll px-4 font-primary"
+			>
+				{/* Fullscreen idle video overlay — now in front of terminal */}
 				{idleEditorVideoPlaying && (
 					<video
 						autoPlay
@@ -218,10 +222,6 @@ export const Editor = () => {
 						className="fixed inset-0 w-screen h-screen object-cover z-[50]"
 					/>
 				)}
-			<div
-				id="editor-root"
-				className="fixed h-screen max-h-screen w-full overflow-scroll px-4 font-primary"
-			>
 				<div className="relative mx-auto h-full max-w-screen">
 					<EditorHeader />
 					<div className="relative m-0 mx-auto p-0 h-full">
@@ -229,7 +229,7 @@ export const Editor = () => {
 					</div>
 				</div>
 			</div>
-			{isEditor && isLoaded&& <EditorFooter />}
+			{isEditor && isLoaded&& !idleEditorVideoPlaying && <EditorFooter />}
 		</>
 	);
 };
