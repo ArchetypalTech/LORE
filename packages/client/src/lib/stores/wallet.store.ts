7f42d2d3140bd42a3bd7439b3dfb7b7e60dd6fff
RM3 <roger@studioavante.com>
2026-02-11 12:21:55 -0300

    chore: upgrade controller 0.13.5

--- packages/client/src/lib/stores/wallet.store.ts
+++ packages/client/src/lib/stores/wallet.store.ts
@@ -54,7 +54,7 @@ const setupController = async () => {
 		policies: {
 			contracts: {
 				[LORE_CONFIG.contractAddresses.actions_token]: {
-					name: worldName, // Optional, can be added if you want a name
+					// name: worldName, // Optional, can be added if you want a name
 					description: `Aprove consume / clame of game tokens in ${worldName}`,
 					methods: [
 						{
@@ -76,7 +76,7 @@ const setupController = async () => {
 					],
 				},
 				[LORE_CONFIG.contractAddresses.prompt]: {
-					name: worldName, // Optional, can be added if you want a name
+					// name: worldName, // Optional, can be added if you want a name
 					description: `Aprove submitting transactions to ${worldName}`,
 					methods: [
 						{
@@ -86,7 +86,7 @@ const setupController = async () => {
 					],
 				},
 				[LORE_CONFIG.contractAddresses.designer]: {
-					name: APP_EDITOR_DATA.title, // Optional, can be added if you want a name
+					// name: APP_EDITOR_DATA.title, // Optional, can be added if you want a name
 					description: `Aprove submitting transactions to ${APP_EDITOR_DATA.title} when using the editor tool`,
 					methods: [
 						{
@@ -234,6 +234,7 @@ const setupController = async () => {
 	};
 
 	try {
+		console.log("DEBUG: controllerConfig", controllerConfig);
 		const controller = new Controller(controllerConfig);
 		console.log("DEBUG: controller", controller);
 		set({
